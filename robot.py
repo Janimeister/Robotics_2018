ca6a1e4db23cb87149cf3e3c71b8532b16189ecb
Jani Heikkinen <janiheikkinen94@gmail.com>
2018-02-02 10:11:03 +0200

    Fixes

--- robot.py
+++ robot.py
@@ -2,14 +2,11 @@ import serial
 import time
 
 def main():
-
     connected = False
     ser = serial.Serial("COM4", 9600)
     if ser:
         connected = True
-        ser.write(bytes(b'1'))
-    time.sleep(5)
+        while (connected):
+            ser.write(b'1')
     ser.close()
-
-
 main()
