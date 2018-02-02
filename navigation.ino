0a1c09cbfae3575265ecde555a978cc515ca32d9
Jani Heikkinen <janiheikkinen94@gmail.com>
2018-02-02 10:11:45 +0200

    Fixes

--- navigation.ino
+++ navigation.ino
@@ -10,7 +10,6 @@ void setup()
 {
   AFMS.begin();
   Serial.begin(9600);
-  //Serial.write('1');
 }
 
 void loop()
@@ -19,13 +18,17 @@ void loop()
   {
     if (Serial.read() == '1')
     {
-      while (Serial.read() != 0)
-      {
         myMotor1 ->setSpeed(150);
         myMotor2 ->setSpeed(150);
         myMotor1 ->run(FORWARD);
         myMotor2 ->run(FORWARD);
-      }
+    }
+    if (Serial.read() == '0')
+    {
+        myMotor1 ->setSpeed(0);
+        myMotor2 ->setSpeed(0);
+        myMotor1 ->run(BRAKE);
+        myMotor2 ->run(BRAKE);
     }
   }
 }
