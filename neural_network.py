af80e4217a67958b9b8431c162dbcfa9451a36d3
Jani Heikkinen <janiheikkinen94@gmail.com>
2018-02-23 10:23:29 +0200

    NN modifications

--- neural_network.py
+++ neural_network.py
@@ -15,11 +15,9 @@ def main():
 def nn_initialize():
     model = Sequential()
     # Input layer
-    model.add(Dense(units=4, input_dim=self.state_size, activation='relu'))
+    model.add(Dense(units=3, input_dim=16, activation='sigmoid'))
     #Hidden layer
-    model.add(Dense(units=8, activation='relu'))
-    #Hidden layer
-    model.add(Dense(units=8, activation='relu'))
+    model.add(Dense(units=32, activation='sigmoid'))
     #Output layer
     model.add(Dense(units=4, activation='sigmoid'))
     #Compiling model
@@ -28,19 +26,37 @@ def nn_initialize():
     return model
 
 def nn_train(model, state):
+    alpha = 0.001
+    highest_prediction = nn_predict(model)
+    gamma = highest_prediction / 3
+
+    state = data
+    Qvalue = highest_prediction + alpha * (reward + gamma + nn_predict(model, state))
+
     #Train the model with one iteration and input (state)
     model.fit(state, reward_value, epochs=1, verbose=0)
 
     return model
 
-def nn_predict(model):
+def nn_predict(model, state):
     #Make new prediction
     prediction = model.predict(state)
+    highest_prediction = np.amax(prediction)
+    #take highest value from prediction
+    #calculate q value
+    #highest prediction + alpha (0.001) * (reward + gamma * new prediction)
+    #first high gamma, doesn't work so good on long term as low
+    
+    #take prediction with previous state, calculate reward
 
-    return (model)
+    #take action, return only it. put above to train. nn_predict only for saving previous values.
+    
+    #rules over here!
+    
+    return (highest_prediction)
 
 def dummy_data():
-    dummy = np.random.randint(0,200, size=(4,1))
+    dummy = np.random.random_sample((4,0))
     
     return dummy
 
@@ -50,12 +66,12 @@ def data(sensor1, sensor2, sensor3, sensor4):
     sensor3 = 3
     sensor4 = 4
 
-    state = [[sensor1],
-             [sensor2],
-             [sensor3],
-             [sensor4]]
-
+    state = [sensor1,
+             sensor2,
+             sensor3,
+             sensor4]
+    #change states to 0-1, no higher values allowed
     return state
 main()
 
-
+#reward also -1   0    1
