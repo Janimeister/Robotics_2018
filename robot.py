2bb474466ce58c42d25aa4063da7c3c31224a99f
Julumettu <juhoronkko@hotmail.com>
2018-03-01 11:55:27 +0000

    Robot movement

--- robot.py
+++ robot.py
@@ -5,9 +5,18 @@ def main():
 	ser = serial.Serial("/dev/rfcomm0", 9600)
 	while True:
 		print("Reading from serial: ")
-		data = (ser.readline().strip())
-		data = data.decode("utf-8")
-		lista = data.split(',')
-		print(lista)
-
+		try:
+			data = (ser.readline().strip())
+			data = data.decode("utf-8")
+			lista = data.split(',')
+			#Lisätään lista printtauksen lisäksi neuroverkolle
+			print(lista)
+			#Tietojen perusteella lähetetään vastaus neuroverkolta
+			#Tyylillä ser.write("neuroverkonvastaus")
+			var = input("Lähetä jotakin arduinolle(1-4): ")
+			ser.write(var.encode())
+		except:
+			print("Something was wrong with the data...")
+			var2 = input("Haluatko varmasti lopettaa?: ")
+			if var2 == "y": quit()
 main()
