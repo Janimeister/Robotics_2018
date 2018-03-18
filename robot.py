906244755553f44febc286a055f73633bb3c173a
Jani Heikkinen <janiheikkinen94@gmail.com>
2018-03-18 20:38:20 +0200

    Added comments

--- robot.py
+++ robot.py
@@ -4,10 +4,12 @@ import time
 def main():
     model = neural_network.initialize()
     data = 0
+    #Open serial port between Arduino and Raspberry Pi
     ser = serial.Serial("/dev/rfcomm0", 9600)
     while True:
             print("Reading from serial: ")
             try:
+                #Read sensor values
                 bt_data = (ser.readline().strip())
                 bt_data = bt_data.decode("utf-8")
                 
@@ -16,23 +18,24 @@ def main():
                 
                 print("Read data is: ", s_list)
                 
-                #Lisätään lista printtauksen lisäksi neuroverkolle
-                
+                #Send the sensor readings to neural network
                 state = neural_network.data(s_list)
                 time.sleep(1.5)
                 
-                
+                #Read new sensor values
                 bt_data = (ser.readline().strip())
                 bt_data = bt_data.decode("utf-8")
                 
                 s_list = bt_data.split(',')
                 s_list = list(map(int, s_list))
                 
+                #Send the sensor readings to neural network
                 state_new = neural_network.data(s_list)
-                
+                #Call training function in NN to get next action (direction)
                 model, action = neural_network.nn_train_sensor(model, state, state_new)
                 time.sleep(0.5)
                 print("Action is: " ,action)
+                #Send desired action to Arduino
                 ser.write(str(action).encode())
                 time.sleep(0.5)
 
